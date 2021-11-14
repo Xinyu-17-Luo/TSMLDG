d9399befe6b911700f5c2063b5f044f2635702f9
Xinyu-17-Luo <83354761+Xinyu-17-Luo@users.noreply.github.com>
2021-11-14 21:34:26 +0100

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ parser.add_argument('--target', default='C', help='target domain name, only one 
 parser.add_argument('--inner-lr', type=float, default=1e-3, help='inner learning rate of meta update')
 parser.add_argument('--outer-lr', type=float, default=5e-3, help='outer learning rate of network update')
 parser.add_argument('--resume', action='store_true', help='resume the training procedure')
-parser.add_argument('--debug', action='store_true', help='set the workers=0 and batch size=1 to accelerate debug')
+# parser.add_argument('--debug', action='store_true', help='set the workers=0 and batch size=1 to accelerate debug') # MetaFrameWork() doesn't have this parameter
 parser.add_argument('--train-size', type=int, default=8, help='the batch size of training')
 parser.add_argument('--test-size', type=int, default=16, help='the batch size of evaluation')
 parser.add_argument('--train-num', type=int, default=1,
@@ -21,8 +21,9 @@ def train():
     args = vars(parser.parse_args())
     print(args)
     for name in args['source']:
-        assert name in 'GSIMCcuv'
-    assert args['target'][0] in  'GSIMCcuv'
+        # my datasets
+        assert name in 'WBIACD'
+    assert args['target'][0] in  'WBIACD'
     assert len(args['target'])
     framework = MetaFrameWork(**args)
     framework.do_train()
@@ -30,6 +31,7 @@ def train():
 
 if __name__ == '__main__':
     from utils.task import FunctionJob
+    # parameter to be changed gpus, minimum_memory
     job = FunctionJob([train], gpus=[[3, 1, 2, 0]])
     job.run(minimum_memory=20000)
     # train()
